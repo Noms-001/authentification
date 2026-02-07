3f7d4739b05b7e2432e7890d2f7d0a0aedf43c05
Noms-001 <randrichris17@gmail.com>
2026-02-07 18:33:05 +0300

    Update AuthController.java
    expose api for register

--- src/main/java/com/example/authentification/controller/AuthController.java
+++ src/main/java/com/example/authentification/controller/AuthController.java
@@ -1,7 +1,10 @@
 package com.example.authentification.controller;
 
 import com.example.authentification.dto.ApiResponse;
+import com.example.authentification.dto.UtilisateurDTO;
+import com.example.authentification.entity.Utilisateur;
 import com.example.authentification.service.AuthService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -35,6 +38,17 @@ public class AuthController {
         } catch (Exception e) {
             ApiResponse<String> response = ApiResponse.error(e.getMessage());
             return ResponseEntity.ok(response);
+        }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse<UtilisateurDTO>> register(@RequestParam String email, @RequestParam String nom, @RequestParam String password) {
+        try {
+            Utilisateur user = authService.register(email, password, nom);
+            UtilisateurDTO dto = new UtilisateurDTO(user.getId(), user.getEmail(), user.getNom(), user.getRole());
+            return ResponseEntity.ok(ApiResponse.success(dto));
+        } catch (Exception e) {
+            return ResponseEntity.ok(ApiResponse.error(e.getMessage()));
         }
     }
 }
