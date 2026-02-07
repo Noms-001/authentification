1d7164ed0a7f7b3a6f88d72f04299288521ba3db
Noms-001 <myahaintso@gmail.com>
2026-02-07 15:11:41 +0300

    Create ApiResponse.java

---
+++ src/main/java/com/example/authentification/dto/ApiResponse.java
@@ -0,0 +1,46 @@
+package com.example.authentification.dto;
+
+public class ApiResponse<T> {
+    private boolean success;
+    private T data;
+    private String error;
+
+    public ApiResponse(boolean success, T data, String error) {
+        this.success = success;
+        this.data = data;
+        this.error = error;
+    }
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(true, data, null);
+    }
+
+    public static <T> ApiResponse<T> error(String message) {
+        return new ApiResponse<>(false, null, message);
+    }
+
+    // getters et setters
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+}
