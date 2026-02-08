24370965f98c0b0a679ac1bb40d06aba1911353c
Noms-001 <randrichris17@gmail.com>
2026-02-08 07:51:26 +0300

    Update UtilisateurRepository.java
    create function findByEmailOrName

--- src/main/java/com/example/authentification/repository/UtilisateurRepository.java
+++ src/main/java/com/example/authentification/repository/UtilisateurRepository.java
@@ -2,11 +2,18 @@ package com.example.authentification.repository;
 
 import com.example.authentification.entity.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, String> {
     Optional<Utilisateur> findByEmail(String email);
+
+    @Query("SELECT u FROM Utilisateur u WHERE u.email LIKE %:keyword% OR u.nom LIKE %:keyword%")
+    List<Utilisateur> findByEmailContainingOrNomContaining(@Param("keyword") String keyword);
+    
 }
