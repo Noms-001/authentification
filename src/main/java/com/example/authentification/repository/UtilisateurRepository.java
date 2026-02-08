afc77a40b2d7556d60600ebd280f61951ecc2bf1
Noms-001 <randrichris17@gmail.com>
2026-02-08 08:09:34 +0300

    Update UtilisateurRepository.java
    add findisBlockedTrue

--- src/main/java/com/example/authentification/repository/UtilisateurRepository.java
+++ src/main/java/com/example/authentification/repository/UtilisateurRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, String> {
     Optional<Utilisateur> findByEmail(String email);
+    List<Utilisateur> findByIsBlockedTrue();
 }
